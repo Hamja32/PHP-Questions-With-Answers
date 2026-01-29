2b945ca81809ea9c32617ac171e9cd41980aa62b
Hamza khan <srk123boss@gmail.com>
2026-01-29 12:28:17 +0530

    Assignment 1 Q1,Q2 done

---
+++ ass1q1.php
@@ -0,0 +1,21 @@
+<?php
+# Q1 - Write a PHP program to define a user define function that accept two numbers and returns their sum,subtraction,multiplication and division.
+
+#code
+function  printCalculation($a, $b){
+$sum = $a + $b;
+$subtract = $a - $b;
+$multiplication = $a * $b;
+$division = $a / $b;
+
+$result = ["sum"=>$sum,"subtract"=>$subtract,"multiplication"=>$multiplication,"division"=>$division];
+return $result;
+}
+
+$values = printCalculation(10,5);
+foreach($values as $key => $val){
+    echo "<br>";
+    echo "$key = $val";
+}
+
+?>
---
+++ ass1q2.php
@@ -0,0 +1,12 @@
+<?php
+
+# Q2 - Create a PHP program using an indexed array to store the names of 10 students and display them using a loop ?
+
+#code
+$students = array("Hamza","Shaan","Ovesh","Tayyab","Zeeshan","Aman","Sher Mohammed","Altaf","Arman","Hitesh");
+for ($i=0; $i < sizeof($students); $i++) { 
+    echo "<br>";
+    echo "$i : $students[$i]";
+}
+
+?>
